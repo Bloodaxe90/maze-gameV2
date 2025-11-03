daa958608476acf4197a37ce23e1caad610236f7
matsmadgaming <matthewdurham96@gmail.com>
2025-11-03 12:29:48 +0000

    Added some functions to dean

--- core/src/main/java/io/github/eng1group9/entities/Dean.java
+++ core/src/main/java/io/github/eng1group9/entities/Dean.java
@@ -1,6 +1,9 @@
 package io.github.eng1group9.entities;
 
+import java.util.List;
+
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 public class Dean extends AnimatedEntity {
@@ -11,5 +14,27 @@ public class Dean extends AnimatedEntity {
         setPosition(startPos);
         setScale(2);
     }
+
+    public void setSpeed(float newSpeed) {
+        speed = newSpeed;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    private boolean safeToMove(float x, float y, List<Rectangle> collisionRectangles) {
+        Rectangle testHitbox = new Rectangle();
+        testHitbox = testHitbox.set(getHitbox());
+        testHitbox.setPosition(x + 16, y + 16);
+
+        for (Rectangle rectangle : collisionRectangles) {
+            if (rectangle.overlaps(testHitbox)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
